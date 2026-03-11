bea892bc708bc6320b4a8a6711aa53051217045b
Michael Berardi <michael-berardi@users.noreply.github.com>
2026-03-11 08:03:27 -0400

    Fix SEO audit findings

--- app/robots.ts
+++ app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/api/', '/private/'],
+      disallow: ['/api/', '/_next/', '/private/'],
     },
     sitemap: 'https://buenosairesexpats.com/sitemap.xml',
   };
